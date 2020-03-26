9861809cac1299e1d82a2369e9dc0a0744114dd7
ミント <minto@mintonoMacBook-Pro.local>
2020-03-26 21:05:17 +0900

    fix_users_table

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2020_03_25_121958) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname"
-    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
