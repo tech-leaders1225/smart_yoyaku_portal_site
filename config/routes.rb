186f6b67ed813e27438b08191393701829bfa94e
shotaimai66 <changemymind6@gmail.com>
2020-03-30 15:59:46 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,25 @@
 Rails.application.routes.draw do
-
-
+  
   # devise↓ =====================================================================================
-  devise_for :admins
-  get 'admins/top', to: 'admins#top'
-  devise_for :masseurs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
+    devise_for :admins
+    devise_for :masseurs
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :users, controllers: {
+      sessions:      'users/sessions',
+      passwords:     'users/passwords',
+      registrations: 'users/registrations'
+    }
 
 
   # User↓========================================================================================
-  root "top#index"
-  get "/shop", to: "top#shop"
-  get "/details", to: "top#details"
+    root "top#index"
+    get "/shop", to: "top#shop"
+    get "/details", to: "top#details"
 
   
 
   # Admin↓========================================================================================
-  
+    get 'admins/top', to: 'admins#top'
 
   # Masseur↓========================================================================================
 
