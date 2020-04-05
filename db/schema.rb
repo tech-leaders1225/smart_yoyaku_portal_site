8944804f0bd393b8a32694e513664d89300d3bd5
ミント <minto@mintonoMacBook-Pro.local>
2020-04-05 15:08:45 +0900

    rails_update_6.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_053244) do
+ActiveRecord::Schema.define(version: 2020_04_04_054155) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
