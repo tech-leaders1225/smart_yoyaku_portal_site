3edbd242ebe690dab7abd08d9b1d18ecb679ccef
ミント <minto@mintonoMacBook-Pro.local>
2020-04-30 11:56:37 +0900

    fix_colomun_add_create_table

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,5 @@
 class Store < ApplicationRecord
+  belongs_to :store_manager
   has_many :masseur, dependent: :destroy
+  has_many :plan, dependent: :destroy
 end
---
+++ spec/factories/store_managers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :store_manager do
+    
+  end
+end
