e1487958db09dfdf247d1e67c9a24ae8212f4f69
makino-tsukasa <aoitori.toyohashi@gmail.com>
2020-05-01 14:34:18 +0900

    fix_view_for_users/sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  # deviceを使用したuserのsign_upに使用
+  def configure_permitted_parameters
+    added_attrs = [ :name, :email, :password, :password_confirmation, :nickname, :address, :gender ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_083406) do
+ActiveRecord::Schema.define(version: 2020_04_30_081721) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 2020_04_08_083406) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
