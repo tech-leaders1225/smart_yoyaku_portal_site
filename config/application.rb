72a3a96abedeb13264752db01fe2fbdedfd06897
ryoji125 <48088374+ryoji125@users.noreply.github.com>
2020-05-20 12:26:51 +0900

    Revert "Feat/google login"

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module SmartReservation
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
