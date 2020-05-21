4cf5dff3c205c158661cb60dec4dfa147ed57ffd
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-21 02:27:30 +0000

    newページ完成 create機能は写真機能を除いてほぼ完成

--- app/controllers/store_manager/plans_controller.rb
+++ app/controllers/store_manager/plans_controller.rb
@@ -4,6 +4,24 @@ class StoreManager::PlansController < StoreManager::Base
     end
     
     def new
+      @plan = Plan.new
+    end
+    
+    def create
+      @plan = Plan.new(plan_params)
+      if @plan.save
+        flash[:success] = '新規作成に成功しました。'
+        redirect_to store_manager_plans_path
+      else
+        flash[:danger] = '入力情報に誤りがありました。もう一度入力情報を確認して下さい。'
+        render :new
+      end
+    end
+    
+    private
+  
+    def plan_params
+      params.require(:plan).permit(:store_id, :plan_name, :plan_content, :plan_time, :plan_price)
     end
         
     
