22929bfe8ad443feed71f5c388e2ec0500e125a6
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-22 15:46:14 +0000

    プル修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,11 @@
 Rails.application.configure do
-  config.hosts.clear
-  
-  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "4e4f46ca4b3a4ef899198d33ffe60ac0.vfs.cloud9.us-east-2.amazonaws.com"
-  config.hosts << "abe2cde6d21244b980a1cf420e2e3229.vfs.cloud9.us-east-2.amazonaws.com"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
