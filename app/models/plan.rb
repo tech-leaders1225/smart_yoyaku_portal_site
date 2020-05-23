05ff90cd8e21c5ff230dd122d8d38baef63d6c6d
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-23 22:55:18 +0000

    バリデーション設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,10 @@
 class Plan < ApplicationRecord
   belongs_to :store
   has_many :plan_images 
+  
+  validates :plan_name, presence: true, length: { in: 1..35 }
+  validates :plan_content, presence: true, length: { in: 1..500 }
+  validates :plan_price, presence: true, length: { maximum: 99999 }
+  validates :plan_time, presence: true, length: { maximum: 999 }
+  
 end
