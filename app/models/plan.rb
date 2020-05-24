82a6fe83e57549e4d9e401742ffab6e9a4bcdac6
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-24 10:34:50 +0000

    plan new index show 修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,7 +3,7 @@ class Plan < ApplicationRecord
   has_many :plan_images 
   
   validates :plan_name, presence: true, length: { in: 1..35 }
-  validates :plan_content, presence: true, length: { in: 1..500 }
+  validates :plan_content, presence: true, length: { in: 1..300 }
   validates :plan_price, presence: true, length: { maximum: 99999 }
   validates :plan_time, presence: true, length: { maximum: 999 }
   
