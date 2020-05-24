c04a93c86ba070f4b86f81eb71f2f91e1a9f91b4
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-24 11:40:07 +0000

    プルリク前修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ApplicationRecord
   belongs_to :store
-  has_many :plan_images 
+  has_many :plan_images, dependent: :destroy
   
   validates :plan_name, presence: true, length: { in: 1..35 }
   validates :plan_content, presence: true, length: { in: 1..300 }
