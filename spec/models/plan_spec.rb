759aabe95724cd8381ba0731b6c0e22498e05e5d
松本 <ec2-user@ip-172-31-44-184.us-east-2.compute.internal>
2020-05-25 22:15:54 +0000

    plan_spec.rb test_code

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @plan = build(:plan)
+  end
+  
+  it "store_idがなければ無効であること" do
+    @plan.store_id = nil
+    expect(@plan).to be_invalid
+  end
+  
+  describe 'masseur_name' do
+    it "プラン名が無い場合、無効であること" do
+      @plan.name = nil
+      expect(@plan).to be_invalid
+    end
+    it "プラン内容が無い場合、無効であること" do
+      @plan.plan_content = nil
+      expect(@plan).to be_invalid
+    end
+    it "プラン時間が無い場合、無効であること" do
+      @plan.plan_time = nil
+      expect(@plan).to be_invalid
+    end
+    it "プラン料金が無い場合、無効であること" do
+      @plan.plan_price = nil
+      expect(@plan).to be_invalid
+    end
+  end
 end
