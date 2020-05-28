f5f9b9c8a1616f50010d6024cf47c3f828e3cb65
Ubuntu <ubuntu@ip-172-31-19-85.us-east-2.compute.internal>
2020-05-28 14:38:24 +0000

    詳細画面修正２

--- app/controllers/user/top_controller.rb
+++ app/controllers/user/top_controller.rb
@@ -8,9 +8,6 @@ class User::TopController < User::Base
 
   def details
     @store = Store.find(params[:id])
-    # @masseur = Masseur.find(params[:id])
     @plans = @store.plan
-    @masseurs = @store.masseur.all
-    @reviews = Review.all
   end
 end
