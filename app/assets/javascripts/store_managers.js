9464e94277af7e7c2e3593ffa3fa68b8d236e085
EC2 Default User <ec2-user@ip-172-31-39-74.us-east-2.compute.internal>
2020-06-20 08:20:50 +0000

    一旦カレンダー表示

--- app/assets/javascripts/store_managers.js
+++ app/assets/javascripts/store_managers.js
@@ -2,3 +2,22 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require_tree ./store_managers
+//= require moment
+//= require fullcalendar
+
+$(function () {
+    function eventCalendar() {
+        return $('#calendar').fullCalendar({});
+    };
+    function clearCalendar() {
+        $('#calendar').html('');
+    };
+    $(document).on('turbolinks:load', function () {
+    eventCalendar();
+    });
+    $(document).on('turbolinks:before-cache', clearCalendar);
+
+    $('#calendar').fullCalendar({
+    events: '/events.json'
+    });
+});
