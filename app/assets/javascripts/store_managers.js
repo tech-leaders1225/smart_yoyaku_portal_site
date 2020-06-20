48d39500850d583e99b4fde8d5ead1b37590cabd
EC2 Default User <ec2-user@ip-172-31-39-74.us-east-2.compute.internal>
2020-06-20 09:01:11 +0000

    カレンダーオプション設定中

--- app/assets/javascripts/store_managers.js
+++ app/assets/javascripts/store_managers.js
@@ -5,19 +5,48 @@
 //= require moment
 //= require fullcalendar
 
-$(function () {
-    function eventCalendar() {
-        return $('#calendar').fullCalendar({});
-    };
-    function clearCalendar() {
-        $('#calendar').html('');
-    };
-    $(document).on('turbolinks:load', function () {
-    eventCalendar();
-    });
-    $(document).on('turbolinks:before-cache', clearCalendar);
-
-    $('#calendar').fullCalendar({
-    events: '/events.json'
-    });
+$('#calendar').fullCalendar({
+        // ヘッダーのタイトルとボタン
+        header: {
+            // title, prev, next, prevYear, nextYear, today
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month agendaWeek agendaDay'
+        },
+        dayClick: function () {
+            //ここにモーダルで詳細表示
+            alert('日付クリックイベント');
+        },
+        // イベントソース(ここにAPI情報を記載)
+        eventSources: [
+            {
+                events: [
+                    {
+                        title: 'event1',
+                        start: '2020-06-02'
+                    },
+                    {
+                        title: 'event2',
+                        start: '2013-01-02',
+                        end: '2013-01-03'
+                    },
+                    {
+                        title: 'event3',
+                        start: '2013-01-05 12:30:00',
+                        allDay: false // will make the time show
+                    }
+                ]
+            }
+        ],
+        
+        // ボタン文字列
+        buttonText: {
+            today:    '今日',
+            month:    '月',
+            week:     '週',
+            day:      '日'
+        },
+        
 });
+    
+   
