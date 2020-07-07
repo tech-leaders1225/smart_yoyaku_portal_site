206e4a6316ed2e5352ba141c9fd524771b34dbd1
makino-tsukasa <aoitori.toyohashi@gmail.com>
2020-07-07 22:06:14 +0900

    fix_flash_message

--- app/controllers/store_manager/top_page_controller.rb
+++ app/controllers/store_manager/top_page_controller.rb
@@ -3,7 +3,8 @@ class StoreManager::TopPageController < StoreManager::Base
   def top
     @store_manager = current_store_manager
     if @store_manager.order_plan.nil?
-      flash.now[:notice] = "現在ご利用中のプランは【無料プラン】となっております。「登録内容の編集」から有料プランをご契約いただきますと全ての機能をご利用いただくことができます。"
+      flash.now[:notice] = "現在ご利用中のプランは【無料プラン】となっており、#{@store_manager.store.store_name}様のページは非公開となっております。<br>
+                            「登録内容の編集」から有料プランをご契約いただきますとページが公開され、お客様からのご予約が可能となります。".html_safe
     end
   end
 end
