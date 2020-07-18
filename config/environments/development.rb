adc8422e005423dfecdfc6c8c67d5038e096673c
EC2 Default User <ec2-user@ip-172-31-1-178.us-east-2.compute.internal>
2020-07-18 11:03:19 +0000

    出張範囲作り直し

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts << "40b370c940fc440ca35551e34d084508.vfs.cloud9.us-east-2.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
