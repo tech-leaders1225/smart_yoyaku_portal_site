5ee2a59e6ef6d76dae034c9583531d4c7cba5585
ミント <minto@mintonoMacBook-Pro.local>
2020-07-19 11:50:52 +0900

    fix_docker_file_fix_spec

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :plan do
     sequence(:plan_name) { |n| "plan_name#{n}" }
     sequence(:plan_content) { |n| "plan_content#{n}" }
-    sequence(:plan_time){ |n| "1#{n}" }
-    sequence(:plan_price){ |n| "2#{n}" }
+    sequence(:plan_time){ |n| "#{n}" }
+    sequence(:plan_price){ |n| "#{n}" }
 
     after(:build) do |plan|
       plan.store = create(:store)
--- spec/requests/store_manager/plans_request_spec.rb
+++ spec/requests/store_manager/plans_request_spec.rb
@@ -48,29 +48,28 @@ RSpec.describe "StoreManager::Plans", type: :request do
     end
   end
   
-  describe "POST /create" do
-    context "store_managerがログイン中の場合" do
-      it "入力内容が正しい場合、planが新規作成されること" do
-        # plan_params = FactoryBot.attributes_for(:plan)
-        # sign_in @store_manager
-        # post store_manager_plans_path(@plan), params: { plan: plan_params }
-        # expect(response.status).to eq 302
-      end
-      it "入力内容が不正な場合、planが新規作成されないこと" do
-        # plan_params = FactoryBot.attributes_for(:plan, plan_name: "文字列オーバーです"*30)
-        # sign_in @store_manager
-        # post store_manager_plans_path(@plan), params: { plan: plan_params }
-        # expect(@plan.reload.plan_name).to_not eq "文字列オーバーです"*30
-        # expect(response.status).to eq 200
-      end
-    end
-  end
+  # describe "POST /create" do
+  #   context "store_managerがログイン中の場合" do
+  #     it "入力内容が正しい場合、planが新規作成されること" do
+  #       sign_in @store_manager
+  #       post store_manager_plans_path(@plan)
+  #       expect(response.status).to eq 200
+  #     end
+  #     it "入力内容が不正な場合、planが新規作成されないこと" do
+  #       @plan = @plan.update( plan_name: "文字列オーバーです"*30)
+  #       sign_in @store_manager
+  #       post store_manager_plans_path(@plan)
+  #       expect(@plan.reload.plan_name).to_not eq "文字列オーバーです"
+  #       expect(response.status).to eq 302
+  #     end
+  #   end
+  # end
   
   describe "GET /show" do
     context "store_managerがログイン中の場合" do
-      it "同じstoreを持つplanのshowが表示されること" do
+      it "storeがplanのshowが表示されること" do
         sign_in @store_manager
-        get store_manager_plan_path(@store_manager)
+        get store_manager_plan_path(@plan)
         expect(response).to have_http_status(:success)
         expect(response).to render_template :show
         expect(response.status).to eq 200
@@ -82,7 +81,7 @@ RSpec.describe "StoreManager::Plans", type: :request do
     context "store_managerがログイン中の場合" do
       it "同じstoreを持つplanのeditが表示されること" do
         sign_in @store_manager
-        get edit_store_manager_plan_path(@store_manager)
+        get edit_store_manager_plan_path(@plan)
         expect(response).to have_http_status(:success)
         expect(response).to render_template :edit
         expect(response.status).to eq 200
