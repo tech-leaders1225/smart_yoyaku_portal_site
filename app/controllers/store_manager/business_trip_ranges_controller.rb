0aad241757405be0227e6c785da8ca8af793165d
EC2 Default User <ec2-user@ip-172-31-1-178.us-east-2.compute.internal>
2020-07-21 06:42:52 +0000

    showページ表示完了

--- app/controllers/store_manager/business_trip_ranges_controller.rb
+++ app/controllers/store_manager/business_trip_ranges_controller.rb
@@ -3,14 +3,14 @@ class StoreManager::BusinessTripRangesController < StoreManager::Base
   include StoreManager::BusinessTripRangesHelper
   
   # before_action :corrrect_store_manager, only: [:edit, :update]
-  before_action :set_masseur, only: [:edit, :update]
-  
-  attr_accessor :city_ids
+  before_action :set_masseur, only: [:edit, :update, :show]
   
   def index
     @store = current_store_manager.store
     @masseurs = @store.masseurs
   end
+  
+  
   
   def edit
     @prefectures = Prefecture.all
@@ -28,6 +28,14 @@ class StoreManager::BusinessTripRangesController < StoreManager::Base
       flash[:success] = "出張範囲を選択してください。"
       redirect_to store_manager_masseurs_business_trip_ranges_url
     end
+  end
+  
+  def show
+    @business_trip_ranges = @current_masseur.business_trip_ranges
+    # 閲覧中のマッサージ師が持っている出張範囲(市町村)が属している都道府県IDを配列にして取得
+    @prefecture_ids = @current_masseur.business_trip_ranges.pluck(:city_id).map {|id| City.find_by(id: id).prefecture_id } 
+    # 閲覧中のマッサージ師が持っているcity_idを配列で取得
+    @city_ids = @current_masseur.business_trip_ranges.pluck(:city_id)
   end
   
   private
