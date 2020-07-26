a8cb85f2454d3ef008a5f5a5bbccee7c494ab020
EC2 Default User <ec2-user@ip-172-31-1-178.us-east-2.compute.internal>
2020-07-26 03:49:05 +0000

    save

--- db/migrate/20200718113820_create_cities.rb
+++ db/migrate/20200718113820_create_cities.rb
@@ -3,7 +3,7 @@ class CreateCities < ActiveRecord::Migration[6.0]
     create_table :cities do |t|
       t.string :name
       t.references :prefecture, null: false, foreign_key: true
-
+      
       t.timestamps
     end
   end
--- db/migrate/20200718115840_create_business_trip_ranges.rb
+++ db/migrate/20200718115840_create_business_trip_ranges.rb
@@ -3,7 +3,7 @@ class CreateBusinessTripRanges < ActiveRecord::Migration[6.0]
     create_table :business_trip_ranges do |t|
       t.references :masseur, null: false, foreign_key: true
       t.references :city, null: false, foreign_key: true
-
+      
       t.timestamps
     end
   end
