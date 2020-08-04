f176e4d6247623f5dd42c90260d7d0e4d8890c81
松本隼 <matsumotoshun@matsumotoshunnoMacBook-Pro.local>
2020-08-04 18:40:22 +0900

    save

--- app/controllers/user/top_controller.rb
+++ app/controllers/user/top_controller.rb
@@ -18,12 +18,11 @@ class User::TopController < User::Base
   def details
     @reserve_app_url = reserve_app_url
     @plans = @store.plans
-    @store.masseurs
+    @masseurs = @store.masseurs
     @store_images = @store.store_images.first
     unless @store_images.nil?
       @count_store_image = @store_images.store_image.count
     end
-
   end
 
   private
