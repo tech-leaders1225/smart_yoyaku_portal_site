06ebf79e0c89c69ae0397913c495d83bf2c7ac1d
松本隼 <matsumotoshun@matsumotoshunnoMacBook-Pro.local>
2020-08-06 23:21:59 +0900

    プルリク

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_store_attrs
   end
 
-  # 一般ユーザーがログイン中に新規作成ページに行こうとするとマイページに遷移。
-  # store_managerがログイン中にstore_manager新規作成ページに行こうとするとrootに遷移。
+  # 一般ユーザーがログイン中に新規作成ページに行こうとするとマイページに遷移
+  # store_managerがログイン中にstore_manager新規作成ページに行こうとするとrootに遷移
   def after_sign_in_path_for(resource)
     if store_manager_signed_in?
       root_path
