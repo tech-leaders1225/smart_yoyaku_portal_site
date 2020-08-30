519656ac376f23999d0731a070c6dc69ff107ffe
EC2 Default User <ec2-user@ip-172-31-33-16.us-east-2.compute.internal>
2020-08-30 03:04:09 +0000

    英語エラー

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+config.hosts << "10bb57eeb10b45fc88b29fc8d3d973a1.vfs.cloud9.us-east-2.amazonaws.com"
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
